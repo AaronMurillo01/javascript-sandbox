3c23bc76202db931731795cc6b9d3e63eb17ce4d
Aaron Murillo <amurillo11@cnm.edu>
2023-10-12 10:39:40 -0600

    Adding Data Types Practice

---
+++ 01-variables-data-types/04-data -types/script.js
@@ -0,0 +1,42 @@
+//String
+const firstName = 'Sara';
+
+//Number
+const age = 30;
+const temp = 98.9;
+
+//Boolean
+const hasKids = true;
+
+//Null
+const aptNumber = null;
+
+//undefined
+// let score; 
+const score = undefined;
+
+//Symbol
+const id = Symbol ('id')
+
+//bigInt
+const n = 9007199254740991n;
+
+//Reference Types
+const numbers = [1, 2, 3, 4];
+
+const person = {
+    name: 'Brad'
+};
+
+function sayHello() {
+    console.log('Hello')
+}
+
+const output = sayHello;
+
+
+console.log(output, typeof output)
+
+
+
+
