848d8bdce730d2ac1aaf8ea094e409b012af03b7
Aaron Murillo <amurillo11@cnm.edu>
2023-10-17 16:06:47 -0600

    Adding Date and Times Practices

---
+++ 01-variables-data-types/14-date-and-time/script.js
@@ -0,0 +1,23 @@
+let d;
+
+d = new Date();
+
+d = d.toString();
+
+d = new Date(2021, 6, 10, 12, 30, 0);
+
+d = new Date('2021-07-10T12:30:00');
+d = new Date('07/10/2021 12:30:00');
+d = new Date('07-10-2022');
+
+d = Date.now();
+
+d = new Date();
+d = d.getTime();
+d = d.valueOf();
+
+d = new Date(1242141232)
+
+d = Math.floor(Date.now() / 1000);
+
+console.log(d);
