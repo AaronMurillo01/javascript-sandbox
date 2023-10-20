5c683552d46a5df0f96da78c1f4f3cfd9121f45b
Aaron Murillo <amurillo11@cnm.edu>
2023-10-19 20:51:53 -0600

    Adding The Call Stack Practice

---
+++ 03-functions-scope/11-call-stack/script.js
@@ -0,0 +1,17 @@
+//First Example
+
+function first() {
+    console.log('first...');
+}
+
+function second() {
+    console.log('second...');
+}
+
+function third() {
+    console.log('third...');
+}
+
+first();
+second();
+third();
