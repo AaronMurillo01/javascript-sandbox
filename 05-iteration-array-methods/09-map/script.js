6ef3a039db11ca296eae01b83b50d368fde86470
Aaron Murillo <amurillo11@cnm.edu>
2023-10-23 22:08:46 -0600

    Adding Map Practice

---
+++ 05-iteration-array-methods/09-map/script.js
@@ -0,0 +1,14 @@
+const numbers = [1, 2, 3, 4, 5];
+
+const doubleNumbers = numbers.map((number) => number * 2);
+
+console.log(doubleNumbers);
+
+// Same with forEach
+const doubleNumbers2 = [];
+
+numbers.forEach((number) => {
+    doubleNumbers2.push(number * 2);
+});
+
+console.log(doubleNumbers2);
