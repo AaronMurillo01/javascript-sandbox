b76083de6622c41debce0fb228648c1c62526103
Aaron Murillo <amurillo11@cnm.edu>
2023-10-30 15:39:39 -0600

    Adding Remove & Clear Items

--- 08-shopping-list-project/script.js
+++ 08-shopping-list-project/script.js
@@ -1,6 +1,7 @@
 const itemForm = document.getElementById("item-form");
 const itemInput = document.getElementById("item-input");
 const itemList = document.getElementById("item-list");
+const clearBtn = document.getElementById("clear");
 
 function addItem(e) {
   e.preventDefault();
@@ -21,7 +22,7 @@ function addItem(e) {
   li.appendChild(button);
 
   itemList.appendChild(li);
-  itemInput.value = '';
+  itemInput.value = "";
 }
 
 function createButton(classes) {
@@ -38,5 +39,20 @@ function createIcon(classes) {
   return icon;
 }
 
+function removeItem(e) {
+  if (e.target.parentElement.classList.contains("remove-item"));
+  {
+    e.target.parentElement.parentElement.remove();
+  }
+}
+
+function clearItem () {
+    while (itemList.firstChild) {
+        itemList.removeChild(itemList.firstChild);
+    }
+}
+
 //Event Listeners
 itemForm.addEventListener("submit", addItem);
+itemList.addEventListener("click", removeItem);
+clearBtn.addEventListener("click", clearItem);
