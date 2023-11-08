b9758c827c1caec922a1b9b977d7f5644ab04784
Aaron Murillo <amurillo11@cnm.edu>
2023-11-08 16:43:35 -0700

    Adding Async & Await Error Handling Practice

---
+++ 10-fetch-and-async-await/08-async-await-error-handling/script.js
@@ -0,0 +1,36 @@
+// const getUsers = async () => {
+//     try {
+//         // const response = await fetch('https://jsonplaceholder.typicode.com/users');
+//         const response = await fetch('https://httstat.us/500')
+
+//         if (!response.ok) {
+//             throw new Error('Request Failed');
+//         }
+
+//         const data = await response.json();
+
+//         console.log(data)
+//     } catch (error) {
+//         console.log(error)
+//     }
+// }
+
+// getUsers();
+
+const getPosts = async () => {
+    try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/users');
+        // const response = await fetch('https://httstat.us/500')
+
+        if (!response.ok) {
+            throw new Error('Request Failed');
+        }
+
+        const data = await response.text();
+
+        console.log(data)
+    } catch (error) {
+        console.log(error)
+    }
+getPosts().catch(error => console.log(error))}
+
