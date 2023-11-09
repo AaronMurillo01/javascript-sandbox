dfadbac65038722b5f4d9a5c24a7f876b4dee7b5
Aaron Murillo <amurillo11@cnm.edu>
2023-11-09 16:27:42 -0700

    Adding sync/Await Multiple Promises

--- 10-fetch-and-async-await/08-async-await-error-handling/script.js
+++ 10-fetch-and-async-await/08-async-await-error-handling/script.js
@@ -17,20 +17,20 @@
 
 // getUsers();
 
-const getPosts = async () => {
-    try {
-        const response = await fetch('https://jsonplaceholder.typicode.com/users');
-        // const response = await fetch('https://httstat.us/500')
+// const getPosts = async () => {
+//     try {
+//         const response = await fetch('https://jsonplaceholder.typicode.com/users');
+//         // const response = await fetch('https://httstat.us/500')
 
-        if (!response.ok) {
-            throw new Error('Request Failed');
-        }
+//         if (!response.ok) {
+//             throw new Error('Request Failed');
+//         }
 
-        const data = await response.text();
+//         const data = await response.text();
 
-        console.log(data)
-    } catch (error) {
-        console.log(error)
-    }
-getPosts().catch(error => console.log(error))}
+//         console.log(data)
+//     } catch (error) {
+//         console.log(error)
+//     }
+// getPosts().catch(error => console.log(error))}
 
